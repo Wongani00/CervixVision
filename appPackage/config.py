e67acf4cb289f7c0620817544ea8989c6042ec29
Wongani Nyirenda <159172209+Wongani00@users.noreply.github.com>
2025-07-13 22:09:51 -1200

    Update config.py

--- appPackage/config.py
+++ appPackage/config.py
@@ -3,16 +3,14 @@ import os
 
 # app configurations
 class Config:
-    SECRET_KEY = "wuidhfw78y3r32r323ru23t3"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///cervical_cancer.db"
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     WTF_CSRF_ENABLED = True
     TF_ENABLE_ONEDNN_OPTS = 0
     MAIL_SERVER = "smtp.googlemail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = "wonganikaunga726@gmail.com"  # os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = "lidq djoo hdue fdvm"  # os.environ.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = (
-        "wonganikaunga726@gmail.com"  # os.environ.get('MAIL_DEFAULT_SENDER')
-    )
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
