b7b5b319223e7421124cdb35d35c05df14f268ec
Wongani Nyirenda <159172209+Wongani00@users.noreply.github.com>
2025-07-28 18:23:15 -1200

    Update run.py

--- run.py
+++ run.py
@@ -9,4 +9,5 @@ app = create_app()
 #     print("All tables recreated.")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # fallback to 10000
+    app.run(host="0.0.0.0", port=port)
