1ed223780c6133d371eb84fa5c9918d808afbfd5
Wongani Nyirenda <159172209+Wongani00@users.noreply.github.com>
2025-07-28 18:19:37 -1200

    updating the static folder

--- appPackage/static/script.js
+++ appPackage/static/script.js
@@ -262,6 +262,8 @@ async function handleAddUserSubmit(event) {
       } else {
         successMessage.remove();
       }
+      // reload the page after success
+      location.reload();
     }, 3000);
 
     // 3. Reset the form
@@ -285,6 +287,7 @@ async function handleAddUserSubmit(event) {
       } else {
         errorMessage.remove();
       }
+
     }, 3000);
   }
 }
@@ -847,13 +850,13 @@ document
 //       transform: translateY(0);
 //     }
 //   }
-  
+
 //   @keyframes pulse {
 //     0% { transform: scale(1); }
 //     50% { transform: scale(1.05); }
 //     100% { transform: scale(1); }
 //   }
-  
+
 //   @keyframes bounce {
 //     0%, 100% { transform: translateY(0); }
 //     50% { transform: translateY(-5px); }
